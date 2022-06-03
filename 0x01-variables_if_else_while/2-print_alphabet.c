99da49cc5525e613fd5bef0d5ff3ba6d01452878
charykui <charycharice@gmail.com>
2022-06-03 13:53:23 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,12 +10,8 @@ int main(void)
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
-		char(ch);
-	}
-	for (ch = 'A' ; ch <= 'Z' ; ch++)
-	{
 		putchar(ch);
 	}
-		putchar('\n');
+	putchar('\n');
 	return (0);
 }
