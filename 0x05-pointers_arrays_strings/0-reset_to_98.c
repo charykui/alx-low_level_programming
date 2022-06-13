61a9c2170741c21e645afc596c582eff6d3ce348
charykui <charycharice@gmail.com>
2022-06-13 01:04:15 -0700

    resetting function

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98: function that resets
- * *n is a pointer
+ * reset_to_98 -a function that resets
+ * @n is a pointer
  */
 void reset_to_98(int *n)
 {
 
-	*n=98;
+	*n = 98;
 
 }
